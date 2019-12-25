c03973608943ff82362b50cebd6a0d98fe8b38d8
Sipo Charles <sipocharles@yahoo.co.uk>
2019-12-25 06:53:40 +0000

    updated README

---
+++ chapter_01/hello_world.py
@@ -0,0 +1,19 @@
+### Sipo Charles - 25/12/2019 ###
+
+## Variables ##
+
+message = "Hello Python World!"
+print(message)
+
+message = "Hello Python Crash Course World!"
+print(message)
+
+print("Hello Python world!")
+
+""" Every variable is connected to a 
+value, which is the information associated with that variable. In this case
+the value is the "Hello Python world!" text.
+Adding a variable makes a little more work for the Python interpreter.
+When it processes the first line, it associates the variable message with the
+"Hello Python world!" text. When it reaches the second line, it prints the
+value associated with message to the screen """
--- pt2_variables_and_simple_data_types/strings/simple_message.py
+++
@@ -1,7 +0,0 @@
-# Sipo Charles - 24/12/2019
-
-message = "This is a test!"
-print(message)
-
-second_message = "This is also a test!"
-print(second_message)

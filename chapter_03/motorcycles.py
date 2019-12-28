7832411f25692244f421bca35d36bc12adedf0d7
Sipo Charles <sipocharles@yahoo.co.uk>
2019-12-28 23:58:57 +0000

    updated chapter_03

--- chapter_03/motorcycles.py
+++ chapter_03/motorcycles.py
@@ -8,5 +8,19 @@ print(motorcycles)
 motorcycles[0] = 'ducati'
 print(motorcycles)
 
+## Adding Elements to a List
+
+""" You might want to add a new element to a list for many reasons. For
+example, you might want to make new aliens appear in a game, add new
+data to a visualization, or add new registered users to a website you’ve
+built. Python provides several ways to add new data to existing lists. """
+
+## Appending Elements to the End of a List
+
+""" The simplest way to add a new element to a list is to append the item to the
+list. When you append an item to a list, the new element is added to the end
+of the list. Using the same list we had in the previous example, we’ll add the
+new element 'ducati' to the end of the list: """
+
 """ For additional information, refer to official Python documentation 
 which is located at www.python.org """

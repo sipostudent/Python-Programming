a8af728f7e2a3e8121fe142c8371950b52beca35
Sipo Charles <sipocharles@yahoo.co.uk>
2019-12-30 23:56:33 +0000

    updated chapter_03

---
+++ chapter_03/cars.py
@@ -0,0 +1,7 @@
+### Organizing a List ###
+
+""" Often, your lists will be created in an unpredictable order, because you can’t always control 
+the order in which your users provide their data. Although this is unavoidable in most circumstances, 
+you’ll frequently want to present your information in a particular order. Sometimes you’ll want to 
+preserve the original order of your list, and other times you’ll want to change the original order. 
+Python provides a number of different ways to organize your lists, depending on the situation. """

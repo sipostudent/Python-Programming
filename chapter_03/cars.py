cf125249c6bdf0d11c3d9de3920f645db4152f1c
Sipo Charles <sipocharles@yahoo.co.uk>
2019-12-30 23:58:51 +0000

    updated chatper_03

--- chapter_03/cars.py
+++ chapter_03/cars.py
@@ -5,3 +5,11 @@ the order in which your users provide their data. Although this is unavoidable i
 you’ll frequently want to present your information in a particular order. Sometimes you’ll want to 
 preserve the original order of your list, and other times you’ll want to change the original order. 
 Python provides a number of different ways to organize your lists, depending on the situation. """
+
+
+## Sorting a List Permanently with the sort() Method 
+
+""" Python’s sort() method makes it relatively easy to sort a list. 
+Imagine we have a list of cars and want to change the order of the list 
+to store them alphabetically. To keep the task simple, let’s assume that 
+all the values in the list are lowercase.

2a497504390af7a3a96f82e863d813df0b0c9909
Sipo Charles <sipocharles@yahoo.co.uk>
2019-12-30 23:59:18 +0000

    updated chapter_03

--- chapter_03/cars.py
+++ chapter_03/cars.py
@@ -12,4 +12,4 @@ Python provides a number of different ways to organize your lists, depending on 
 """ Python’s sort() method makes it relatively easy to sort a list. 
 Imagine we have a list of cars and want to change the order of the list 
 to store them alphabetically. To keep the task simple, let’s assume that 
-all the values in the list are lowercase.
+all the values in the list are lowercase. """
